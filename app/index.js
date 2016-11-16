51637916497028675e43ca13451ce1ac0d42266f
Romario Lopez C <romariolopezc@gmail.com>
2016-11-16 14:23:52 -0600

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,16 @@
+var React = require('react');
+
+var ReactDOM = require('react-dom');
+
+var HelloWorld = React.createClass({
+  render: function () {
+    return (
+      <div> Hello ReactJS Program</div>
+    )
+  }
+})
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('app')
+)
